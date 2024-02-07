03787a4c184ce82aec6377f38b423bcaed206fb1
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-07 20:38:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Helper-Botz/MINNALMURALI.git /MINNALMURALI
+  git clone https://github.com/Helper-Botz/NASRANI.git /NASRANI
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MINNALMURALI
+  git clone $UPSTREAM_REPO /NASRANI
 fi
-cd /MINNALMURALI
+cd /NASRANI
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
